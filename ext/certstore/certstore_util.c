cd54f408598862955afd241e6ca55b3191bca05d
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2019-09-19 12:26:51 +0900

    Remove needless xmalloc return variable check
    Because xmalloc returns non-NULL value.
    If it fails to allocate memory, it kicks Ruby GC.
    Even if it fails to allocate memory, it raises exception.

--- ext/certstore/certstore_util.c
+++ ext/certstore/certstore_util.c
@@ -16,7 +16,7 @@ wstr_to_mbstr(UINT cp, const WCHAR *wstr, int clen)
 {
     char *ptr;
     int len = WideCharToMultiByte(cp, 0, wstr, clen, NULL, 0, NULL, NULL);
-    if (!(ptr = xmalloc(len))) return NULL;
+    ptr = xmalloc(len);
     WideCharToMultiByte(cp, 0, wstr, clen, ptr, len, NULL, NULL);
 
     return ptr;
